1cead63c50c9ab6a40c35e0a7fa298a7c8137eed
Aditya kumar soni <adiksoni@gmail.com>
2025-06-14 12:12:04 +0530

    JWT added

--- 01_blog_api/blog/views.py
+++ 01_blog_api/blog/views.py
@@ -2,10 +2,15 @@ from django.shortcuts import render
 from rest_framework import viewsets, permissions
 from .models import Post
 from .serializers import PostSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 # Create your views here.
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [permissions.AllowAny]
+    # permission_classes = [permissions.AllowAny]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
