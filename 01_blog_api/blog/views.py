d7429b0d20d6d1822457cf898cd3e088de0481e2
Aditya kumar soni <adiksoni@gmail.com>
2025-07-08 19:00:40 +0530

    pagination Added

--- 01_blog_api/blog/views.py
+++ 01_blog_api/blog/views.py
@@ -4,6 +4,7 @@ from .models import Post
 from .serializers import PostSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .permissions import IsAuthorOrReadOnly
+from rest_framework import filters
 
 # Create your views here.
 
@@ -13,6 +14,8 @@ class PostViewSet(viewsets.ModelViewSet):
     # permission_classes = [permissions.AllowAny]
     # permission_classes = [IsAuthenticatedOrReadOnly]
     permission_classes = [IsAuthorOrReadOnly]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'content']
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
--- 01_blog_api/blog_project/settings.py
+++ 01_blog_api/blog_project/settings.py
@@ -57,6 +57,17 @@ REST_FRAMEWORK = {
     ),
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ],
+}
+
 
 ROOT_URLCONF = 'blog_project.urls'
 
