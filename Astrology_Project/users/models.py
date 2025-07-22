d0060b3ff4e60d469c5cff0ca4a48b95c94b31a0
Aditya kumar soni <adiksoni@gmail.com>
2025-07-23 01:42:58 +0530

    User model added

--- Astrology_Project/users/models.py
+++ Astrology_Project/users/models.py
@@ -3,3 +3,38 @@ from django.db import models
 
 # Create your models here.
 
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, role='USER', **extra_fields):
+        if not email:
+            raise ValueError('Email is required')
+        email = self.normalize_email(email)
+        user = self.model(email=email, role=role, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password,role='ADMIN', **extra_fields)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    ROLE_CHOISES = (
+        ('USER', 'User'),
+        ('ASTROLOGER', 'Astrologer'),
+        ('ADMIN', 'Admin')
+    )
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=255)
+    role = models.CharField(max_length=20, choices=ROLE_CHOISES, default='USER')
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False) #Required for admin Login
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['full_name']
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
