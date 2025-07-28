a661aa95065d72888f538096852dad909760a4e9
Aditya kumar soni <adiksoni@gmail.com>
2025-07-29 01:00:30 +0530

    view available added

--- Astrology_Project/astrologers/urls.py
+++ Astrology_Project/astrologers/urls.py
@@ -1,11 +1,12 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path,include
-from .views import AstrologerProfileViewSet
+from .views import AstrologerProfileViewSet, PublicAstologerListView
 
 router = DefaultRouter()
 router.register(r'profile', AstrologerProfileViewSet, basename='astrolger-profile')
 
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('available/', PublicAstologerListView.as_view(), name= 'public-astrologer-list')
 ]
--- Astrology_Project/astrologers/views.py
+++ Astrology_Project/astrologers/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets, permissions
 from .serializers import AstrologerProfileSerializer
 from .models import AstrologerProfile
+from rest_framework import generics
 
 # Create your views here.
 
@@ -18,3 +19,24 @@ class AstrologerProfileViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+
+class PublicAstologerListView(generics.ListAPIView):
+    serializer_class = AstrologerProfileSerializer
+
+    def get_queryset(self):
+        queryset = AstrologerProfile.objects.filter(is_approved=True)
+        language = self.request.query_params.get('language')
+        expertise = self.request.query_params.get('expertise')
+        min_price = self.request.query_params.get('min_price')
+        max_price = self.request.query_params.get('max_price')
+
+        if language:
+            queryset = queryset.filter(language_spoken_contains = [language])
+        if expertise:
+            queryset = queryset.filter(expertise__contains = [expertise])
+        if min_price:
+            queryset = queryset.filter(price_per_minute__gte=min_price)
+        if max_price:
+            queryset = queryset.filter(price_per_minute__lte=max_price)
+        return queryset
